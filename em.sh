81a9e1ac65c0a15c2963ddb7620f8ba9f089a5fb
zhanggehui <395510548@qq.com>
2020-12-15 21:33:12 +0800

    update

--- em.sh
+++ em.sh
@@ -3,7 +3,7 @@
 for ((i=0;i<20;i++))
 do
 gmx grompp -f ./scripts/em.mdp  -c GO2-ion.gro -p GO2.top \
--o ./$rundir/em.tpr -po ./$rundir/em-out -n waterlayer.ndx
+-o ./$rundir/em.tpr -po ./$rundir/em-out -n waterlayer.ndx -maxwarn 1
 
 cd $rundir
 $gmxrun -v -deffnm em

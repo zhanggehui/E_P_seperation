c2cf84be6e86adb5a159703b70e48d9a1a673d31
zhanggehui <395510548@qq.com>
2020-12-16 17:23:15 +0800

    update

--- nvt-equ.sh
+++ nvt-equ.sh
@@ -6,6 +6,6 @@
 gmx grompp -f ./$scriptsdir/nvt-equ.mdp -c GO2-afterem.gro -p GO2.top \
 -o ./$rundir/nvt-equ.tpr -po ./$rundir/nvt-equ-out -n waterlayer.ndx -maxwarn 1
 
-cd $rundir ; $gmxrun -v -deffnm nvt-equ -cpt -1
+cd $rundir ; $gmxrun -v -deffnm nvt-equ
 #echo "temperature" | gmx energy -f nvt-equ.edr -o nvt-equ-tem.xvg  #能量输出被关闭
 cd ..

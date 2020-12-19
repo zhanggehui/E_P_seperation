d2610f5ec62f6bcbfaa29497c1fd9bc3ea122d9a
zhanggehui <395510548@qq.com>
2020-12-19 21:45:40 +0800

    update

--- prepare/lay_spacing_pre.sh
+++ prepare/lay_spacing_pre.sh
@@ -1,19 +1,30 @@
 source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 
+poolsimulation=1
+if [ $poolsimulation -eq 1 ]; then
+    dir1=pool
+else
+    dir1=lay_spacing
+fi 
 odir=`pwd`
 ions=("CS" "LI" "NA" "K" "CA" "MG")
 for((ioni=2;ioni<3;ioni++)); do
     ion=${ions[${ioni}]}
     for((layi=7;layi<=13;layi++)); do
         lay=`awk -v layi=$layi 'BEGIN{printf("%2.1f",layi*0.1);}'` 
-        dir=lay_spacing/$ion/$lay
+        dir=$dir1/$ion/$lay
         if [ ! -d "./$dir" ];then
             mkdir -p ./$dir
             cp -r oplsaaGO.ff ./$dir
-            cp ./receive/$ion/$lay/GO_${lay}.gro ./$dir/GO2-ion.gro
-            cp ./receive/$ion/$lay/GO_${lay}.top ./$dir/GO2.top
+            if [ $poolsimulation -eq 1 ]; then
+                dir2=${ion}_pool ; grofile=GO_pool_${lay}.gro ; topfile=GO_pool_${lay}.top
+            else
+                dir2=${ion} ; grofile=GO_${lay}.gro ; topfile=GO_${lay}.top
+            fi
+            cp ./receive/$dir2/$lay/$grofile ./$dir/GO2-ion.gro
+            cp ./receive/$dir2/$lay/$topfile ./$dir/GO2.top
             cd ./$dir
-            gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx < $odir/receive/$ion/$lay/ndx.sh
+            gmx make_ndx -f GO2-ion.gro -o waterlayer.ndx < $odir/receive/$dir2/$lay/ndx.sh
             git clone https://github.com/zhanggehui/Gmx_GO_ubuntu.git
             mv Gmx_GO_ubuntu scripts
             cp -rf $odir/md_scripts/.git/config ./scripts/.git/config

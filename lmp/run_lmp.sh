467a5b242fec5a8216096b8489e94e3245562483
zhanggehui <395510548@qq.com>
2021-01-03 13:41:57 +0800

    update

---
+++ lmp/run_lmp.sh
@@ -0,0 +1,2 @@
+cd /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/ ; gitget ; cd $OLDPWS
+sbatch /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/lmp/sub_lmp.sh /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/lmp/GO.inp

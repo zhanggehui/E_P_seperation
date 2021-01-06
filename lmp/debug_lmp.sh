2e0b7cf63482494499b0dc5c1fb08ed4ebf2661c
zhanggehui <395510548@qq.com>
2021-01-06 19:25:43 +0800

    update

--- lmp/debug_lmp.sh
+++ lmp/debug_lmp.sh
@@ -12,6 +12,7 @@
 # rundir ; runscript
 
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
+export OMP_NUM_THREADS=1
 source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_29Oct2020.sh auto
 cd ./$rundir
 mpirun -np $SLURM_NTASKS lmp -e screen -log none -in $runscript

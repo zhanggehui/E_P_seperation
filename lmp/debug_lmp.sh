aca0f145f9c9a372ba8853c25e82c9eda36b0bdf
zhanggehui <395510548@qq.com>
2021-01-08 20:33:37 +0800

    update

--- lmp/debug_lmp.sh
+++ lmp/debug_lmp.sh
@@ -13,7 +13,7 @@
 
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
 export OMP_NUM_THREADS=1
-source /appsnew/source/intel2018.sh
-#source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_29Oct2020.sh auto
+#source /appsnew/source/intel2018.sh
+source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_29Oct2020.sh auto
 cd ./$rundir
 mpirun -np $SLURM_NTASKS lmp -e screen -log none -in $runscript

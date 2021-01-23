17fcf03a97474979d109bf2c2b74fcce35c6e8c3
zhanggehui <395510548@qq.com>
2021-01-23 13:36:34 +0800

    update

--- auto-run.sh
+++ auto-run.sh
@@ -11,7 +11,6 @@ else
     export scriptsdir='/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx'
 fi
 
-debug_flag=0
 if [ $2 == 'auto' ]; then
     ncnnl=`sinfo | grep 'idle' | grep 'cn_nl' | awk '{print $4}'`
     ncns=`sinfo | grep 'idle' | grep 'cn-short' | awk '{print $4}'`
@@ -35,61 +34,66 @@ elif [ $2 == 'cn-short' ]; then
 elif [ $2 == 'cn_nl' ]; then
     NodeType=cn_nl
 elif [ $2 == 'debug' ]; then
-    debug_flag=1
+    NodeType='debug'
 else
+    NodeType=Unknown
     echo "Unknown NodeType!"
 fi
 
 if [ $NodeType == 'cn_nl' ]; then
     NtasksPerNode=28
-else
+elif [ $NodeType == 'cn-short' ]; then
     NtasksPerNode=20
+else
 fi
 
 #rm -rf $rundir
 if [ ! -d $rundir ]; then
     mkdir $rundir
     ##choose proper node setting ########################################
-    if [ ${debug_flag} -gt 0 ]; then
-        submissionscript="$scriptsdir/debug_${md_code}.sh"
-        NodeType='debug'
+    if [ $NodeType == 'Unknown' ]; then
+        echo "Do nothing!"
     else
-        submissionscript="$scriptsdir/cn_${md_code}.sh"
-        keyword="#SBATCH -p"; newline="#SBATCH -p $NodeType"
-        sed -i "/$keyword/c$newline" $submissionscript
-        keyword="#SBATCH -N"; newline="#SBATCH -N $NodeNum"
-        sed -i "/$keyword/c$newline" $submissionscript
-        keyword="#SBATCH --ntasks-per-node"; newline="#SBATCH --ntasks-per-node=$NtasksPerNode"
-        sed -i "/$keyword/c$newline" $submissionscript
-        if [ "$NodeType" == cn-short ]; then
-            keyword="#SBATCH --qos"; newline="#SBATCH --qos=liufengcns"
+        if [ $NodeType == 'debug' ]; then
+            submissionscript="$scriptsdir/debug_${md_code}.sh"
+        else 
+            submissionscript="$scriptsdir/cn_${md_code}.sh"
+            keyword="#SBATCH -p"; newline="#SBATCH -p $NodeType"
             sed -i "/$keyword/c$newline" $submissionscript
-        elif [ "$NodeType" == cn_nl ]; then
-            keyword="#SBATCH --qos"; newline="#SBATCH --qos=liufengcnnl"
+            keyword="#SBATCH -N"; newline="#SBATCH -N $NodeNum"
             sed -i "/$keyword/c$newline" $submissionscript
-        elif [ "$NodeType" == cn-long ]; then
-            keyword="#SBATCH --qos"; newline="#SBATCH --qos=liufengcnl"
+            keyword="#SBATCH --ntasks-per-node"; newline="#SBATCH --ntasks-per-node=$NtasksPerNode"
             sed -i "/$keyword/c$newline" $submissionscript
+            if [ "$NodeType" == cn-short ]; then
+                keyword="#SBATCH --qos"; newline="#SBATCH --qos=liufengcns"
+                sed -i "/$keyword/c$newline" $submissionscript
+            elif [ "$NodeType" == cn_nl ]; then
+                keyword="#SBATCH --qos"; newline="#SBATCH --qos=liufengcnnl"
+                sed -i "/$keyword/c$newline" $submissionscript
+            elif [ "$NodeType" == cn-long ]; then
+                keyword="#SBATCH --qos"; newline="#SBATCH --qos=liufengcnl"
+                sed -i "/$keyword/c$newline" $submissionscript
+            fi
         fi
+
+        #####################################################################
+        jobname="${md_code}_$4" ; keyword="#SBATCH -J" ; newline="#SBATCH -J $jobname"
+        sed -i "/$keyword/c$newline" $submissionscript
+        oname="./$rundir/1.out" ; keyword="#SBATCH -o" ; newline="#SBATCH -o $oname"
+        sed -i "/$keyword/c$newline" $submissionscript
+        ename="./$rundir/2.err" ; keyword="#SBATCH -e" ; newline="#SBATCH -e $ename"
+        sed -i "/$keyword/c$newline" $submissionscript
+
+        cp $submissionscript ./$rundir
+        cp $scriptsdir/$runscript ./$rundir
+        if [ $runscript == 'nvt-cycle.sh' ]; then
+            cp $scriptsdir/nvt-cycle.mdp ./$rundir
+        fi
+
+        sbatch $submissionscript
+        echo "Submiting a job to ${NodeType}, Please wait..."
+        sleep 2s
     fi
-
-    #####################################################################
-    jobname="${md_code}_$4" ; keyword="#SBATCH -J" ; newline="#SBATCH -J $jobname"
-    sed -i "/$keyword/c$newline" $submissionscript
-    oname="./$rundir/1.out" ; keyword="#SBATCH -o" ; newline="#SBATCH -o $oname"
-    sed -i "/$keyword/c$newline" $submissionscript
-    ename="./$rundir/2.err" ; keyword="#SBATCH -e" ; newline="#SBATCH -e $ename"
-    sed -i "/$keyword/c$newline" $submissionscript
-
-    cp $submissionscript ./$rundir
-    cp $scriptsdir/$runscript ./$rundir
-    if [ $runscript == 'nvt-cycle.sh' ]; then
-        cp $scriptsdir/nvt-cycle.mdp ./$rundir
-    fi
-
-    sbatch $submissionscript
-    echo "Submiting a job to ${NodeType}, Please wait..."
-    sleep 2s
 else
     echo 'Already exists! Please make sure!'
 fi

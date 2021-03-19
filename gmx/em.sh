7d8e2ddad7a6ce7c3e7767c4714a3d00abce653a
zhanggehui <395510548@qq.com>
2021-03-19 22:10:00 +0800

    update

--- gmx/em.sh
+++ gmx/em.sh
@@ -4,7 +4,7 @@
 # orientation ; rundir ; runscript ; scriptsdir
 
 gmx grompp -f $scriptsdir/em.mdp -c GO_ion.gro -p GO_ion_pp.top \
--o ./$rundir/em.tpr -po ./$rundir/em-out -n waterlayer.ndx # -maxwarn 1
+-o ./$rundir/em.tpr -po ./$rundir/em-out -n waterlayer.ndx #-maxwarn 1
 
 cd $rundir ; $gmxrun -v -deffnm em
 # echo "potential" | gmx energy -f em.edr -o em-potential.xvg
--- gmx/nvt-equ.sh
+++ gmx/nvt-equ.sh
@@ -3,8 +3,8 @@
 # environment variable:
 # orientation ; rundir ; runscript ; scriptsdir
 
-gmx grompp -f ./$scriptsdir/nvt-equ.mdp -c GO2-afterem.gro -p GO2.top \
--o ./$rundir/nvt-equ.tpr -po ./$rundir/nvt-equ-out -n waterlayer.ndx -maxwarn 1
+gmx grompp -f $scriptsdir/nvt-equ.mdp -c GO2-afterem.gro -p GO_ion_pp.top \
+-o ./$rundir/nvt-equ.tpr -po ./$rundir/nvt-equ-out -n waterlayer.ndx #-maxwarn 1
 
 cd $rundir ; $gmxrun -v -deffnm nvt-equ
 #echo "temperature" | gmx energy -f nvt-equ.edr -o nvt-equ-tem.xvg  #能量输出被关闭

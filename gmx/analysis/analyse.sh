3adb3fd6aceacb80600bc5b2fca98db1dbd56d81
zhanggehui <395510548@qq.com>
2021-03-22 23:57:59 +0800

    update

---
+++ gmx/analysis/analyse.sh
@@ -0,0 +1,2 @@
+analyse_scripts=do_density.sh
+source ./md_scripts/${analyse_scripts}
--- gmx/analysis/rdf_pv.sh
+++ gmx/analysis/rdf_pv.sh
@@ -3,82 +3,82 @@ ions=("CS" "LI" "NA" "K" "CA" "MG")
 num=${#ions[@]}
 rdfdir=rdfs_pv
 if [ ! -d $rdfdir ] ; then
-mkdir $rdfdir
+    mkdir $rdfdir
 
 for((i=0;i<$num;i++)) ; do
-ion=${ions[$i]}
-mkdir $rdfdir/$ion
+    ion=${ions[$i]}
+    mkdir $rdfdir/$ion
 
-cd ./$ion
+    cd ./$ion
 
-#0Mpa-0V
-cd 0Mpa-0V ; ffile=nvt-pro-traj.trr
-xvgfile=${ion}_0Mpa.xvg
-gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
-gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
-#-bin 0.01 -rmax 1
-cp $xvgfile ../../$rdfdir/$ion
-rm -rf \#*
-cd ../
+    #0Mpa-0V
+    cd 0Mpa-0V ; ffile=nvt-pro-traj.trr
+    xvgfile=${ion}_0Mpa.xvg
+    gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
+    gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
+    #-bin 0.01 -rmax 1
+    cp $xvgfile ../../$rdfdir/$ion
+    rm -rf \#*
+    cd ../
 
-#Mpa
-for ((k=1;k<20;k++)) ; do
-cd ${k}00Mpa-0V ; ffile=nvt-pro-traj.trr
-xvgfile=${ion}_${k}00Mpa.xvg
-gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
-gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
-#-bin 0.01
-cp $xvgfile ../../$rdfdir/$ion
-rm -rf \#*
-cd ../
-done
+    #Mpa
+    for ((k=1;k<20;k++)) ; do
+    cd ${k}00Mpa-0V ; ffile=nvt-pro-traj.trr
+    xvgfile=${ion}_${k}00Mpa.xvg
+    gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
+    gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
+    #-bin 0.01
+    cp $xvgfile ../../$rdfdir/$ion
+    rm -rf \#*
+    cd ../
+    done
 
-#V
-cd 0Mpa-0V ; ffile=nvt-pro-traj.trr
-xvgfile=${ion}_0V.xvg ; ffile=nvt-pro-traj.trr
-gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
-gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
-#-bin 0.01
-cp $xvgfile ../../$rdfdir/$ion
-rm -rf \#*
-cd ../
+    #V
+    cd 0Mpa-0V ; ffile=nvt-pro-traj.trr
+    xvgfile=${ion}_0V.xvg ; ffile=nvt-pro-traj.trr
+    gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
+    gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
+    #-bin 0.01
+    cp $xvgfile ../../$rdfdir/$ion
+    rm -rf \#*
+    cd ../
 
-cd 0Mpa-1V ; ffile=nvt-pro-traj.trr
-xvgfile=${ion}_1V.xvg ; ffile=nvt-pro-traj.trr
-gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
-gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
-#-bin 0.01
-cp $xvgfile ../../$rdfdir/$ion
-rm -rf \#*
-cd ../
+    cd 0Mpa-1V ; ffile=nvt-pro-traj.trr
+    xvgfile=${ion}_1V.xvg ; ffile=nvt-pro-traj.trr
+    gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
+    gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
+    #-bin 0.01
+    cp $xvgfile ../../$rdfdir/$ion
+    rm -rf \#*
+    cd ../
 
-j=0
-for ((k=1;k<10;k++)) ; do
-cd 0Mpa-$j.${k}V ; ffile=nvt-pro-traj.trr
-xvgfile=${ion}_$j.${k}V.xvg
-gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
-gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
-#-bin 0.01
-cp $xvgfile ../../$rdfdir/$ion
-rm -rf \#*
-cd ../
-done
+    j=0
+    for ((k=1;k<10;k++)) ; do
+    cd 0Mpa-$j.${k}V ; ffile=nvt-pro-traj.trr
+    xvgfile=${ion}_$j.${k}V.xvg
+    gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
+    gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
+    #-bin 0.01
+    cp $xvgfile ../../$rdfdir/$ion
+    rm -rf \#*
+    cd ../
+    done
 
-j=1
-for ((k=1;k<7;k++)) ; do
-cd 0Mpa-$j.${k}V ; ffile=nvt-pro-traj.trr
-xvgfile=${ion}_$j.${k}V.xvg
-gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
-gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
-#-bin 0.01
-cp $xvgfile ../../$rdfdir/$ion
-rm -rf \#*
-cd ../
-done
+    j=1
+    for ((k=1;k<7;k++)) ; do
+    cd 0Mpa-$j.${k}V ; ffile=nvt-pro-traj.trr
+    xvgfile=${ion}_$j.${k}V.xvg
+    gmx make_ndx -f last.gro < ../../md_scripts/rdf_ndx.sh
+    gmx rdf -f $ffile -n index.ndx -ref $ion -sel OW -selrpos atom -seltype atom -o $xvgfile -b 0 -e 5000
+    #-bin 0.01
+    cp $xvgfile ../../$rdfdir/$ion
+    rm -rf \#*
+    cd ../
+    done
 
-cd ../
-done
+    cd ../
+    done
 
 else
-echo "already exits!"
+    echo "already exits!"
 fi

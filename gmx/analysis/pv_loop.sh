4b8a29f5ff2b0f5b660702050dcbc9ea68ca66e7
zhanggehui <395510548@qq.com>
2021-03-31 10:09:22 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -4,7 +4,7 @@ cd ../
 cd lay_2.5
 for ((i=0; i<20; i++)); do
     pressure=`awk -v i=$i 'BEGIN{printf("%g",100*i);}'` 
-    for ((j=1; j<17; j++)); do 
+    for ((j=0; j<17; j++)); do 
         voltage=`awk -v j=$j 'BEGIN{printf("%g",0.1*j);}'`
         if [ ${pressure} -eq 0 ] || [ ${voltage} -eq 0 ]; then
             cd ./${ionname}${pressure}Mpa-${voltage}V

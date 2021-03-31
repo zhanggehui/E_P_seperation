c693cfe705774bb2c39ea91ee6c03f877408543a
zhanggehui <395510548@qq.com>
2021-03-31 09:51:54 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -1,17 +1,3 @@
-# 不同条件下的轨迹
-ionname=''
-#charge=26
-#trajdir=charge_${charge}_traj
-# negative=n5
-# trajdir=negative_${negative}_traj
-# spacing=2
-# trajdir=lay_${spacing}_traj
-# rm -rf ../charge_${charge}_traj
-
-# 不同离子的轨迹
-# ionname=MG
-# trajdir=${ionname}_traj
-
 cd ../
 cd lay_2.5
 for ((i=0;i<20;i++)); do
--- gmx/analysis/residence_time.sh
+++ gmx/analysis/residence_time.sh
@@ -1,5 +1,9 @@
-
-mkdir ./select
-cd ./select
-echo -e "\"1st shell\" resname SOL and name OW and within 0.25 of group NA\n" | \
-gmx select -s ../traj.tpr -f ../nvt-pro-traj.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 5000
+if [ ! -d select ]; then
+    mkdir ./select
+    cd ./select
+    echo -e "\"1st shell\" resname SOL and name OW and within 0.25 of group NA\n" | \
+    gmx select -s ../traj.tpr -f ../nvt-pro-traj.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 5000
+    rm -rf \#*
+    cd ../
+fi
+mv ./select ../../$rundir/NA_2.5nm_${pressure}Mpa_${voltage}V

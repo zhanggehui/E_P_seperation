210a4b01188574750060e494d4ec500922accd4f
zhanggehui <395510548@qq.com>
2021-03-31 16:55:30 +0800

    update

--- gmx/analysis/rdf/rdf_func.sh
+++ gmx/analysis/rdf/rdf_func.sh
@@ -1,7 +1,7 @@
 function get_rdf() {
     rm -rf *.xvg
     echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f last.gro
-    gmx rdf -f nvt-pro-traj.trr -n index.ndx -ref $1 -sel OW -selrpos atom -seltype atom -o $2 -b 0 -e 5000 -rmax 1
+    gmx rdf -f nvt-pro-traj.trr -n index.ndx -norm number_density -ref $1 -sel OW -selrpos atom -seltype atom -o $2 -b 0 -e 5000 -rmax 1
     rm -rf \#*
 }
 

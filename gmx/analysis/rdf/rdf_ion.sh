738593ef0882351e1858e011fe87b70796efa73d
zhanggehui <395510548@qq.com>
2021-03-31 16:46:38 +0800

    update

--- gmx/analysis/rdf/rdf_ion.sh
+++ gmx/analysis/rdf/rdf_ion.sh
@@ -3,9 +3,8 @@ cd ../
 for((i=0; i<$n_ions; i++)); do
     ion=${ions[$i]}
     cd ./$ion/0Mpa-0V
-    ffile=nvt-pro-traj.trr
     xvgfile=${ion}_rdf.xvg
-    get_rdf $ffile $ion $xvgfile
+    get_rdf $ion $xvgfile
     mv $xvgfile ../../$rundir
     cd ../../
 done

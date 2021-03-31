d14dbf39284935b33dcccb585f4d439f484cde17
zhanggehui <395510548@qq.com>
2021-03-31 09:56:54 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -2,10 +2,10 @@ cd ../
 cd lay_2.5
 for ((i=0;i<20;i++)); do
     pressure=`awk -v i=$i 'BEGIN{printf("%s",100*i);}'` 
-    for ((i=1;i<17;i++)); do 
-        voltage=`awk -v i=$i 'BEGIN{printf("%s",0.1*i);}'`
+    for ((j=1;j<17;j++)); do 
+        voltage=`awk -v j=$j 'BEGIN{printf("%s",0.1*j);}'`
         if [ ${pressure} -eq 0 ] || [ ${voltage} -eq 0 ]; then
-            source $scriptsdir/.sh
+            source $scriptsdir/residence_time.sh
         fi
     done
 done

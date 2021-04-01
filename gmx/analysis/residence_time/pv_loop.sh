774a4c35275fc966ab720f347701a9bbdd9bc0e5
zhanggehui <395510548@qq.com>
2021-04-01 10:54:22 +0800

    update

--- gmx/analysis/residence_time/pv_loop.sh
+++ gmx/analysis/residence_time/pv_loop.sh
@@ -1,12 +1,13 @@
 cd ../
-cd lay_2.5
+cd 1.1nm
 for ((i=0; i<20; i++)); do
     pressure=`awk -v i=$i 'BEGIN{printf("%g",100*i);}'` 
     for ((j=0; j<17; j++)); do 
         voltage=`awk -v j=$j 'BEGIN{printf("%g",0.1*j);}'`
         if [ ${pressure} -eq 0 ] || [ ${voltage} -eq 0 ]; then
             cd ./${pressure}Mpa-${voltage}V
-            source $scriptsdir/residence_time/residence_time.sh
+            source $scriptsdir/residence_time.sh
+            cd ../
         fi
     done
 done
--- gmx/analysis/residence_time/residence_time.sh
+++ gmx/analysis/residence_time/residence_time.sh
@@ -1,7 +1,7 @@
 if [ ! -d select ]; then
     mkdir ./select
     cd ./select
-    echo -e "\"1st shell\" resname SOL and name OW and within 0.25 of group NA\n" | \
+    echo -e "\"1st shell\" resname SOL and name OW and within 0.24 of group NA\n" | \
     gmx select -s ../traj.tpr -f ../nvt-pro-traj.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 5000
     rm -rf \#*
     cd ../

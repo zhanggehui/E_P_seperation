3336a877497c40be99af3a3969e7387017f5ac54
zhanggehui <395510548@qq.com>
2021-04-14 09:17:29 +0800

    update

---
+++ gmx/ion_loop.sh
@@ -0,0 +1,9 @@
+ions=("LI" "NA" "K" "CS")
+n_ions=${#ions[@]}
+
+for ((i=0;i<$n_ions;i++)); do
+    ion=${ions[$i]}
+    cd $ion
+    gmx auto 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
+    cd ../
+done

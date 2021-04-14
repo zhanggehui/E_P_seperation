1fb650de6f04caff19fd3907abbfc7221d9da2e7
zhanggehui <395510548@qq.com>
2021-04-14 08:52:10 +0800

    update

--- gmx/analysis/rdf/rdf_func.sh
+++ gmx/analysis/rdf/rdf_func.sh
@@ -1,6 +1,5 @@
 function get_rdf_and_coordnum() {
-    #rm -rf *.xvg index.ndx
-    echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f last.gro
-    gmx rdf -f nvt-pro-traj.trr -n index.ndx -norm rdf -ref $1 -sel OW -selrpos atom -seltype atom -o $2 -cn cn.xvg -b 0 -e $3 -rmax 1
+    echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
+    gmx rdf -f nvt-production.trr -n index.ndx -norm rdf -ref $1 -sel OW -selrpos atom -seltype atom -o $2 -cn cn.xvg -b 0 -e $3 -rmax 1
     rm -rf \#* index.ndx
 }
--- gmx/analysis/rdf/rdf_pv.sh
+++ gmx/analysis/rdf/rdf_pv.sh
@@ -1,3 +1,4 @@
+$ion=$rundir
 xvgfile=${pressure}Mpa-${voltage}V.xvg
 get_rdf_and_coordnum $ion $xvgfile 10000
 mv cn.xvg ../$rundir

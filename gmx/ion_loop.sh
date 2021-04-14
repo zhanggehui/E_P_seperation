e0619445a9ab9033668850c8e24abec4f1040a0f
zhanggehui <395510548@qq.com>
2021-04-14 15:44:27 +0800

    update

--- gmx/ion_loop.sh
+++ gmx/ion_loop.sh
@@ -4,7 +4,9 @@ n_ions=${#ions[@]}
 for ((i=0;i<$n_ions;i++)); do
     ion=${ions[$i]}
     cd $ion
-    cd /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/ ; gitget ; cd $OLDPWD ; \
-    gmx auto 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
+    if [ $1 == 'rdf_pv' ]; then
+        cd /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/ ; gitget ; cd $OLDPWD ; \
+        gmx auto 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
+    fi
     cd ../
 done

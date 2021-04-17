bfb40523890e17138ccb209375f95e166b87553f
zhanggehui <395510548@qq.com>
2021-04-17 13:12:40 +0800

    update

--- gmx/analysis/trajectory/traj.sh
+++ gmx/analysis/trajectory/traj.sh
@@ -1,5 +1,6 @@
 # rundir刚好可以与要提取的离子同名
 ion=$rundir
 trajgro=${pressure}Mpa-${voltage}V.gro
-get_first_and_last_frame $ion $trajgro 10000
+#get_first_and_last_frame $ion $trajgro 10000
+get_continuous_frame $ion $trajgro 5000 10000
 mv $trajgro ../$rundir
--- gmx/analysis/trajectory/traj_func.sh
+++ gmx/analysis/trajectory/traj_func.sh
@@ -3,3 +3,9 @@ function get_first_and_last_frame() {
     -o $2 -pbc nojump -b 0 -e $3 -skip $3 -n waterlayer.ndx
     rm -rf \#*
 }
+
+function get_continuous_frame() {
+    echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr \
+    -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
+    rm -rf \#*
+}

5d8eacf8f2112673813a5c61c603f113d051e970
zhanggehui <395510548@qq.com>
2021-04-18 11:51:48 +0800

    update

--- gmx/analysis/residence_time/residence_time.sh
+++ gmx/analysis/residence_time/residence_time.sh
@@ -3,4 +3,4 @@ cd ./select
 echo -e "\"1st shell\" resname SOL and name OW and within 0.32 of group NA\n" | \
 gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 10000
 cd ../
-mv ./select ../$rundir/${pressure}Mpa_${voltage}V
+mv ./select ../$rundir/${pressure}Mpa-${voltage}V

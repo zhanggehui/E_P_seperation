d378a0dda688083d8cc6ffe544d72462db4c6999
zhanggehui <395510548@qq.com>
2021-04-19 23:45:19 +0800

    update

--- gmx/em.sh
+++ gmx/em.sh
@@ -1,3 +1,3 @@
-gmx grompp -f $scriptsdir/em.mdp -c ../GO_ion.gro -p ../GO_ion_pp.top -o em.tpr -n ../waterlayer.ndx #-maxwarn 1
+gmx grompp -f $scriptsdir/em.mdp -c ../GO_ion.gro -p ../GO_ion_pp.top -o em.tpr -n ../waterlayer.ndx -maxwarn 1
 $gmxrun -v -deffnm em
 #echo "potential" | gmx energy -f em.edr -o em-potential.xvg

6e3f8d2f0730e196717495a7e5ed12bc06948adf
zhanggehui <395510548@qq.com>
2021-04-21 14:14:14 +0800

    update

--- gmx/ion_loop.sh
+++ gmx/ion_loop.sh
@@ -1,18 +1,17 @@
 ions=("LI" "NA" "K" "CS")
 n_ions=${#ions[@]}
 
+run_gmx="source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh gmx $2"
+
 for ((i=0;i<4;i++)); do
     ion=${ions[$i]}
     cd ./$ion
     if [ $1 == 'em' ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
-        gmx auto 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx em.sh em
+        $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx em.sh em
     elif [ $1 == 'nvtequ' ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
-        gmx auto 2 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx nvt-equ.sh nvtequ
+        $run_gmx 4 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx nvt-equ.sh nvtequ
     elif [ $1 == 'analysis' ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh \
-        gmx auto 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
+        $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
     fi
     cd ../
 done

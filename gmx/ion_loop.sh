5a00e25a3cec0b82e07e9e681bc19a942411181e
zhanggehui <395510548@qq.com>
2021-04-21 23:34:44 +0800

    update

--- gmx/ion_loop.sh
+++ gmx/ion_loop.sh
@@ -5,13 +5,17 @@ run_gmx="source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh gmx $2"
 
 for ((i=1;i<2;i++)); do
     ion=${ions[$i]}
-    cd ./$ion
-    if [ $1 == 'em' ]; then
-        $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx em.sh em
-    elif [ $1 == 'nvtequ' ]; then
-        $run_gmx 4 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx nvt-equ.sh nvtequ
-    elif [ $1 == 'analysis' ]; then
-        $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
+    if [ -d ./$ion ]; then
+        cd ./$ion
+        if [ $1 == 'em' ]; then
+            $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx em.sh em
+        elif [ $1 == 'nvtequ' ]; then
+            $run_gmx 4 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx nvt-equ.sh nvtequ
+        elif [ $1 == 'analysis' ]; then
+            $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh $ion
+        fi
+        cd ../
+    else
+        "No such dictionary!"
     fi
-    cd ../
 done

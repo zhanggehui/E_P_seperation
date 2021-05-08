1662c20d34f8358caf5c27d00f8805d92c874a65
zhanggehui <395510548@qq.com>
2021-05-08 21:29:15 +0800

    update

--- gmx/analysis/angle/theta.sh
+++ gmx/analysis/angle/theta.sh
@@ -6,18 +6,18 @@
 
 indexes=('4091' '4092' '4093' '4094')
 n_idxs=${#indexes[@]}
-for ((time=0; time<=10; time++)); do
+for ((time=0; time<=0; time++)); do
     for ((i=0; i<${n_idxs}; i++)); do
         index=${indexes[$i]}
         echo -e "resname SOL and name OW and within 0.38 of atomnr ${index}\n" | \
         gmx select -f ../nvt-production.trr -s ../nvt-production.tpr -on ${index}.ndx -b ${time} -e ${time}        
     done
-    # python /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle/read_ion_index.py *.ndx
+    python /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle/read_ion_index.py *.ndx
+    # rm -rf *.ndx
     # gmx gangle -f ../nvt-production.trr -s ../nvt-production.tpr -n ./vector.ndx \
     # -oav av-${time}.xvg -oall av-${time}.xvg -oh av-${time}.xvg -b ${time} -e ${time} \
     # -g1 vector -group1 group vector \
     # -g2 z
-    # rm -rf *.ndx
 done
 
 # cd ../

19e0064a20ecee925b30e148b0ce850611ff1073
zhanggehui <395510548@qq.com>
2021-05-08 22:46:02 +0800

    update

---
+++ gmx/analysis/angle/angle_hist.py
@@ -0,0 +1,52 @@
+import sys
+from pathlib import Path
+
+filenames = []
+
+
+def jump_line(line):
+    return line[0] == ('#' or '@')
+
+
+class all_angle_file:
+
+    def __init__(self, fname):
+        self.fname = fname
+        self.angle_list = []
+        my_file = Path(fname)
+        if my_file.exists():
+            self.__readfile(fname)
+
+    def __readfile(self, fname):
+        with open(fname, 'r') as f:
+            for line in f:
+                if not jump_line(line):
+                    linelist = line.strip().rstrip('\n').split()
+                    for i in range(1, len(linelist)):
+                        self.angle_list.append(float(linelist[i]))
+
+    def extend_angle_list(self, filenames):
+        for i in range(len(filenames)):
+            fname = filenames[i]
+            self.angle_list.extend(all_angle_file(fname).get_angle_list())
+
+    def get_angle_list(self):
+        return self.angle_list
+
+    def write_new(self):
+        with open('all_angle.xvg', 'w') as f:
+            f.write('0.00   ')
+            for angle in self.angle_list:
+                f.write('%.3f   ' % angle)
+            f.write('\n')
+
+
+def all_angle_merge():
+    angle_file = all_angle_file('all_angle.xvg')
+    angle_file.extend_angle_list(filenames)
+    angle_file.write_new()
+
+
+if __name__ == '__main__':
+    filenames = sys.argv[1:len(sys.argv)]
+    all_angle_merge()
--- gmx/analysis/angle/theta.sh
+++ gmx/analysis/angle/theta.sh
@@ -6,19 +6,25 @@
 
 indexes=('4091' '4092' '4093' '4094')
 n_idxs=${#indexes[@]}
-for ((time=0; time<=0; time++)); do
+for ((time=0; time<=100; time++)); do
     for ((i=0; i<${n_idxs}; i++)); do
         index=${indexes[$i]}
         echo -e "resname SOL and name OW and within 0.38 of atomnr ${index}\n" | \
-        gmx select -f ../nvt-production.trr -s ../nvt-production.tpr -on ${index}.ndx -b ${time} -e ${time}        
+        gmx select -quiet -f ../nvt-production.trr -s ../nvt-production.tpr -on ${index}.ndx -b ${time} -e ${time}     
     done
     python /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle/read_ion_index.py *.ndx
-    gmx gangle -f ../nvt-production.trr -s ../nvt-production.tpr -n ./vector.ndx \
+    gmx gangle -quiet -f ../nvt-production.trr -s ../nvt-production.tpr -n ./vector.ndx \
     -oall av-${time}.xvg -b ${time} -e ${time} \
     -g1 vector -group1 'group vector' \
     -g2 z
     #-oav av-${time}.xvg -oh av-${time}.xvg
     rm -rf *.ndx
+
+    n_t=$((time % 100))
+    if [ $n_t -eq 0 ] && [ $time -gt 0 ]; then
+        python /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle/read_ion_index.py av-*.xvg
+        rm -rf av-*.xvg
+    fi
 done
 
 # cd ../

09f02357bf9476ac3ac00a525980073ed1381a6f
zhanggehui <395510548@qq.com>
2021-05-11 10:46:23 +0800

    update

--- gmx/analysis/angle_distribution/theta.sh
+++ gmx/analysis/angle_distribution/theta.sh
@@ -4,11 +4,11 @@ cd ./angle
 
 ion=$rundir
 echo -e "a OW or a ${ion}\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f ../nvt-production.gro
-echo '8' | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o test.gro -pbc nojump -b 1000 -e 10000 -n index.ndx
+echo '8' | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx
 
 dir=/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle_distribution
 
-python $dir/angle_distribution.py test.gro angle_dis.csv ${ion} z
+python $dir/angle_distribution.py ion_OW.gro angle_dis.csv ${ion} z
 rm -rf test.gro
 
 

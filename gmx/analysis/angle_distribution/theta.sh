3a280e939a546ea448e34eb0abfb9d89c54caca6
zhanggehui <395510548@qq.com>
2021-05-12 10:22:37 +0800

    update

--- gmx/analysis/angle_distribution/theta.sh
+++ gmx/analysis/angle_distribution/theta.sh
@@ -1,7 +1,7 @@
 mkdir ./angle
 cd ./angle
 
-echo -e "a OW or a ${ion}\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
+echo -e "a OW or a ${ion}\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
 echo "OW_OR_A_${ion}" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
 
 dir=/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle_distribution

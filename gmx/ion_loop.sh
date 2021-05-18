22b61359f769053e9b67c70b9f76d9de13fb3f0b
zhanggehui <395510548@qq.com>
2021-05-19 00:15:45 +0800

    update

--- gmx/ion_loop.sh
+++ gmx/ion_loop.sh
@@ -13,7 +13,7 @@ for ((i=0; i<${n_ions}; i++)); do
         elif [ $1 == 'nvtequ' ]; then
             $run_gmx 4 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx nvt-equ.sh nvtequ
         elif [ $1 == 'analysis' ]; then
-            $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh ${ion}_rdf
+            $run_gmx 1 /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis pv_loop.sh ${ion}_rest
             #${ion}_angle ${ion}_traj ${ion}_density ${ion}_rdf ${ion}_rest
         fi
         cd ../

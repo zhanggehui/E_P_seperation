6689b447060349e885c8ae511a9c7c101ebf8c0d
zhanggehui <395510548@qq.com>
2021-05-19 23:57:48 +0800

    update

--- gmx/analysis/velocity/velocity_profile.sh
+++ gmx/analysis/velocity/velocity_profile.sh
@@ -1,5 +1,5 @@
 trajgro=${pressure}Mpa-${voltage}V.gro
 get_continuous_frame SOL $trajgro 1000 10000
-source $scriptsdir/velocity/get_velocity_profile.sh z vy $trajgro 4.8
+source $scriptsdir/velocity/get_velocity_profile.sh z vy $trajgro 6
 rm -rf $trajgro
 mv z-vy.xvg ../$rundir/${pressure}Mpa-${voltage}V.xvg

f742781e488b3671a86a3eb4c469dd8b3fbf03c5
zhanggehui <395510548@qq.com>
2021-07-08 09:17:41 +0800

    update

--- gmx/umbrella-script/wham.sh
+++ gmx/umbrella-script/wham.sh
@@ -4,4 +4,4 @@ else
     OMP_NUM_THREADS=20
 fi
 
-gmx wham -it ../tpr-files.dat -if ../pullf-files.dat -o -hist -unit kCal
+gmx wham -it ../tpr-files.dat -if ../pullf-files.dat -is ../coordsel.dat -o -hist -unit kCal

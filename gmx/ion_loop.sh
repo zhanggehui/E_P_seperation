fb19b98aede8fe4b9db4ad39c1df5cfa11b967d3
zhanggehui <395510548@qq.com>
2021-07-12 20:34:03 +0800

    update

--- gmx/ion_loop.sh
+++ gmx/ion_loop.sh
@@ -1,5 +1,5 @@
-#ions=("LI" "NA" "K" "CS")
-ions=("LI")
+#ions=("LI")
+ions=("LI" "NA" "K" "CS" "CA" "MG")
 n_ions=${#ions[@]}
 
 run_gmx="source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh gmx $2"

d001da0e436592aedf3f79fb11ac03c9cb94963e
zhanggehui <395510548@qq.com>
2021-07-16 21:38:26 +0800

    update

--- gmx/umbrella-script/springs.sh
+++ gmx/umbrella-script/springs.sh
@@ -4,6 +4,6 @@ mkdir springs
 for udir in `ls | grep umbrella`; do 
   cd $udir
   fnum=${udir##*-}
-  echo 15 | gmx trjconv -s ./umbrella$fnum.tpr -f ./umbrella$fnum.trr -o ../springs/${fnum}conf${fnum}.gro -n ../../waterlayer.ndx
+  echo 14 | gmx trjconv -s ./umbrella$fnum.tpr -f ./umbrella$fnum.trr -o ../springs/${fnum}conf${fnum}.gro -n ../../waterlayer.ndx
   cd ../.
 done

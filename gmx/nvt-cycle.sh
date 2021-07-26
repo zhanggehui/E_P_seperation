4826b1aa61846b963acf76781ef3a4e15fec9ed6
zhanggehui <395510548@qq.com>
2021-07-26 21:01:20 +0800

    update

--- gmx/nvt-cycle.sh
+++ gmx/nvt-cycle.sh
@@ -1,4 +1,4 @@
-#在rundir中运行
+# 在rundir中运行
 orientation=y
 ncycles=1    
 nsteps=10000000
@@ -14,7 +14,7 @@ cp ../waterlayer.ndx ./waterlayer.ndx
 mdpdir=./mdps ; mkdir -p $mdpdir
 ndxdir=./ndxs ; mkdir -p $ndxdir
 
-mdpfile=./nvt-cycle.mdp
+mdpfile=$scriptsdir/nvt-spring.mdp  # ./nvt-cycle.mdp
 ndxfile=./waterlayer.ndx 
 topfile=../GO_ion_pp.top
 
@@ -30,7 +30,7 @@ if [ $pressure -eq 0 ] || [ $acc_water == 'all' ]; then
     nsteps=`awk -v ncycles=$ncycles -v nsteps=$nsteps 'BEGIN{printf("%s",nsteps*ncycles);}'`
     break_flag=1
 fi
-#修改每个循环的总步数（模拟时长）
+# 修改每个循环的总步数（模拟时长）
 reset_nsteps="nsteps                   = $nsteps"
 sed -i "/nsteps/c${reset_nsteps}" $mdpfile
 
@@ -46,13 +46,13 @@ for ((i=1;i<=$ncycles;i++)); do
     reset_tinit="tinit                    = $tinit"
     sed -i "/tinit/c${reset_tinit}" $mdpfile
 
-    #重新给水加速度,更新mdp文件,更新ndx文件
+    # 重新给水加速度,更新mdp文件,更新ndx文件
     if [ $pressure -gt 0 ]; then
         source $scriptsdir/pressure_on_water.sh
     fi
     echo "######################################### This is the ${i}th grompp  #########################################" >> ./2.err
 
-    #-c必须提供，但只有当cpt文件中没有信息会使用-c的gro文件，即使top没有改变也要提供-p的top文件
+    # -c必须提供，但只有当cpt文件中没有信息会使用-c的gro文件，即使top没有改变也要提供-p的top文件
     gmx grompp -f $mdpfile -c $lastgro -t $lastcpt -p $topfile -o $tprname.tpr -po $mdpdir/step$i -n $ndxfile -maxwarn 1 
 
     echo "########################################## This is the ${i}th run ##########################################" >> ./2.err
@@ -62,20 +62,20 @@ for ((i=1;i<=$ncycles;i++)); do
             break
         fi
     else
-        #分开输出到不同文件，以part.XXXX结尾，补0方法awk 'BEGIN{printf("%04d\n",100)}'
-        #$gmxrun -v -deffnm $tprname -cpi $lastcpt -cpt 120 -noappend
+        # 分开输出到不同文件，以part.XXXX结尾，补0方法awk 'BEGIN{printf("%04d\n",100)}'
+        # $gmxrun -v -deffnm $tprname -cpi $lastcpt -cpt 120 -noappend
         
-        #连续输出到一个文件
+        # 连续输出到一个文件
         $gmxrun -v -deffnm nvt-production -cpi nvt-production.cpt -cpt 120
     fi  
 done
 
-#删除多余的输出文件
+# 删除多余的输出文件
 rm -rf \#*
-#拼接轨迹，由于可以连续输出，不再需要
+# 拼接轨迹，由于可以连续输出，不再需要
 # gmx trjcat -f *.trr -o nvt-pro-traj.trr
 
-#维护之前的文件名接口
+# 维护之前的文件名接口
 # mv ./nvt-production.gro ./last.gro
 # mv ./nvt-production.tpr ./traj.tpr
 # mv ./nvt-production.trr ./nvt-pro-traj.trr

8525cd9ee0994a7c86c48ea0c6962c4bb243c054
zhanggehui <395510548@qq.com>
2021-08-01 21:13:29 +0800

    update

--- gmx/analysis/angle_distribution/theta.sh
+++ gmx/analysis/angle_distribution/theta.sh
@@ -2,16 +2,16 @@ mkdir ./angle
 cd ./angle
 
 # echo -e "a OW or a ${ion}\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
-# echo "OW_OR_A_${ion}" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 30000 -n index.ndx -quiet
+# echo "OW_OR_A_${ion}" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
 
-echo -e "a OW | a 4131\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
-echo "OW_a_4131" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 50000 -n index.ndx -quiet
+echo -e "a OW | a 3948\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
+echo "OW_a_3948" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 50000 -n index.ndx -quiet
 
 
 dir=/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle_distribution
 
-#python $dir/angle_distribution.py ion_OW.gro phix.csv phix.mat ${ion} phix
-#python $dir/angle_distribution.py ion_OW.gro phiy.csv phiy.mat ${ion} phiy
+# python $dir/angle_distribution.py ion_OW.gro phix.csv phix.mat ${ion} phix
+# python $dir/angle_distribution.py ion_OW.gro phiy.csv phiy.mat ${ion} phiy
 python $dir/angle_distribution.py ion_OW.gro theta.csv out.mat ${ion} theta
 rm -rf \#* index.ndx ion_OW.gro
 

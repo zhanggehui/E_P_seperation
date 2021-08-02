499d9026663b6160cef549714f9abcb546b6fd4d
zhanggehui <395510548@qq.com>
2021-08-02 18:20:29 +0800

    update

--- gmx/analysis/angle_distribution/theta.sh
+++ gmx/analysis/angle_distribution/theta.sh
@@ -5,7 +5,7 @@ cd ./angle
 # echo "OW_OR_A_${ion}" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
 
 echo -e "a OW | a 3948\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
-echo "OW_a_3948" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
+echo "OW_a_3948" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 5000 -n index.ndx -quiet
 
 
 dir=/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle_distribution
@@ -14,7 +14,6 @@ dir=/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle_distrib
 # python $dir/angle_distribution.py ion_OW.gro phiy.csv phiy.mat ${ion} phiy
 python $dir/angle_distribution.py ion_OW.gro theta.csv out.mat ${ion} theta
 rm -rf \#* index.ndx ion_OW.gro
-
 
 cd ../
 mv ./angle ../$rundir/${pressure}Mpa-${voltage}V

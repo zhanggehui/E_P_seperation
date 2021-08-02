836791130a421e7abb2ed572347e171527698e38
zhanggehui <395510548@qq.com>
2021-08-02 21:41:30 +0800

    update

--- gmx/ion_loop.sh
+++ gmx/ion_loop.sh
@@ -19,7 +19,7 @@ for ((i=0; i<${n_ions}; i++)); do
         elif [ $1 == 'nvtspring' ]; then
             $run_gmx 2 $gitdir/gmx nvt-cycle.sh spring-1500Mpa-0V
         elif [ $1 == 'analysis' ]; then
-            $run_gmx 1 $gitdir/gmx/analysis pv_loop.sh test #${ion}_angle_merge
+            $run_gmx 1 $gitdir/gmx/analysis pv_loop.sh  ${ion}_angle
             # ${ion}_angle ${ion}_traj ${ion}_density ${ion}_rdf ${ion}_rest ${ion}_vel
         fi
         cd ../

9eb5822e4bfc89b7cb4457ff4e9ec9bc8b4b8836
zhanggehui <395510548@qq.com>
2021-08-03 19:41:49 +0800

    update

--- gmx/analysis/angle_distribution/theta.sh
+++ gmx/analysis/angle_distribution/theta.sh
@@ -5,16 +5,16 @@ cd ./angle
 echo -e "a OW | a 3948\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
 
 dir=/home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx/analysis/angle_distribution
-for ((i=0; i<10; i=i+1)); do
-    tb=$((i*5000))
-    te=$(((i+1)*5000))
+for ((a=0; a<10; a=a+1)); do
+    tb=$((a*5000))
+    te=$(((a+1)*5000))
 
     # echo "OW_OR_A_${ion}" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
-    echo "OW_a_3948" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW_$i.gro -pbc nojump -b ${tb} -e ${te} -n index.ndx -quiet
+    echo "OW_a_3948" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW_$a.gro -pbc nojump -b ${tb} -e ${te} -n index.ndx -quiet
 
     # python $dir/angle_distribution.py ion_OW.gro phix.csv phix.mat ${ion} phix
     # python $dir/angle_distribution.py ion_OW.gro phiy.csv phiy.mat ${ion} phiy
-    python $dir/angle_distribution.py ion_OW_$i.gro theta_$i.csv out_$i.mat ${ion} theta
+    python $dir/angle_distribution.py ion_OW_$a.gro theta_$a.csv out_$a.mat ${ion} theta
 done
 
 python $dir/merge_mat.py out_*.mat

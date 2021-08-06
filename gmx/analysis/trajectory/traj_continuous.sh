cb11ecf4347c22f78459a79cf6fd51b6c16f8322
zhanggehui <395510548@qq.com>
2021-08-06 23:29:44 +0800

    update

--- gmx/analysis/trajectory/traj_continuous.sh
+++ gmx/analysis/trajectory/traj_continuous.sh
@@ -1,3 +1,3 @@
 trajgro=${pressure}Mpa-${voltage}V.gro
-get_continuous_frame SOL $trajgro 5000 10000
+get_continuous_frame SOL $trajgro 0 10000
 mv $trajgro ../$rundir/
--- gmx/analysis/trajectory/traj_func.sh
+++ gmx/analysis/trajectory/traj_func.sh
@@ -1,11 +1,15 @@
 function get_first_and_last_frame() {
-    echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b 0 -e $3 -skip $3 -n waterlayer.ndx
+    echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b 0 -e $3 -skip $3 -n index.ndx
     rm -rf \#*
 }
 
 function get_continuous_frame() {
-    #echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
-    echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
+    # echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
+    # echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
+
+    echo 'z { 4 to 6 }' | gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
+    gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
+
     rm -rf \#* 
     if [ -d index.ndx ]; then
         rm -rf index.ndx

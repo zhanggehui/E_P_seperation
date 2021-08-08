7e260e61fe53079828af8f115314a494acae3d04
zhanggehui <395510548@qq.com>
2021-08-08 23:39:16 +0800

    update

--- gmx/analysis/trajectory/traj_func.sh
+++ gmx/analysis/trajectory/traj_func.sh
@@ -7,7 +7,7 @@ function get_continuous_frame() {
     # echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
     # echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
 
-    echo 'z { 4.1 to 4.9 }' | gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
+    echo 'name OW and z { 3 to 7 }' | gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
     gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n index.ndx
 
     rm -rf \#* 

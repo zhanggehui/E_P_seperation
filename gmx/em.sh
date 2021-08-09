d4c05a72bb278ec1eff9b8d62e57939df981e4ca
zhanggehui <395510548@qq.com>
2021-08-09 15:39:58 +0800

    update

--- gmx/em.sh
+++ gmx/em.sh
@@ -1,2 +1,2 @@
-gmx grompp -f $scriptsdir/em.mdp -c ../GO_ion.gro -p ../GO_ion_pp.top -o em.tpr -n ../waterlayer.ndx #-maxwarn 1
+gmx grompp -f $scriptsdir/em.mdp -c ../GO_ion.gro -p ../GO_ion_pp.top -o em.tpr -n ../waterlayer.ndx # -maxwarn 1
 $gmxrun -v -deffnm em

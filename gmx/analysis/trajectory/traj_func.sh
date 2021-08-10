5ba9ce2dd8bc9ab5520fe71b6e679d94daf50c87
zhanggehui <395510548@qq.com>
2021-08-10 21:25:40 +0800

    update

--- gmx/analysis/trajectory/traj_func.sh
+++ gmx/analysis/trajectory/traj_func.sh
@@ -8,7 +8,8 @@ function get_continuous_frame() {
     # echo "$1" | gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
 
     # echo 'name OW and z { 3 to 7 }' z在3-7nm之间的OW分子
-    echo 'name OW NA' | gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
+    # echo "name OW NA"
+    echo 'name OW' | gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
     gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -b $3 -e $4 -n index.ndx -dt 10 -pbc nojump
 
     rm -rf \#* 

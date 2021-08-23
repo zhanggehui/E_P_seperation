f430d521498ee1c221731f51b65fa0b387af982f
zhanggehui <395510548@qq.com>
2021-08-23 10:54:41 +0800

    update

--- gmx/cycle_sub.sh
+++ gmx/cycle_sub.sh
@@ -30,7 +30,7 @@ for ((i=plow_i; i<=pmax_i; i++)); do
         new="electric-field-$orientation         = ${e_amplitude} 0 0 0" 
         sed -i "/$word/c$new" $scriptsdir/nvt-cycle.mdp
         # rm -rf ${pressure}Mpa-${e_amplitude}V
-        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh gmx $5 20 \
+        source /home/liufeng_pkuhpc/lustre2/zgh/sub_job/auto_run.sh gmx $5 12 \
         /home/liufeng_pkuhpc/lustre3/zgh/GO_MD/md_scripts/gmx nvt-cycle.sh \
         ${pressure}Mpa-${e_amplitude}V
         # spring-${pressure}Mpa-${e_amplitude}V

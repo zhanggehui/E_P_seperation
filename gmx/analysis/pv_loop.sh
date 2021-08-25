c5a41c36085d2935410d099f3502a3094fb27401
zhanggehui <395510548@qq.com>
2021-08-25 09:07:28 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -4,6 +4,7 @@ source $scriptsdir/rdf/rdf_func.sh
 
 cd ../
 
+source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 # 正确启动python环境，由于使用了intel编译器的环境，破坏了原有的python环境
 source deactivate
 source /home/liufeng_pkuhpc/anaconda3/bin/activate base

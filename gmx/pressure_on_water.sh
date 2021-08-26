454ba53d123bff1a53d52ab4bd2f2f9ec09ee2f9
zhanggehui <395510548@qq.com>
2021-08-26 22:01:18 +0800

    update

--- gmx/pressure_on_water.sh
+++ gmx/pressure_on_water.sh
@@ -14,8 +14,8 @@ mv $oldndxfile $ndxdir
 
 # 根据具体情况记得修改acc_water，all/notall
 boxlengthline=`sed -n '$p' $lastgro`
-awk -v boxlengthline="$boxlengthline" \
--v orientation=$orientation -v pressure=$pressure -v i=$i \
+awk -v boxlengthline="$boxlengthline" -v i=$i \
+-v orientation=$orientation -v pressure=$pressure \
 '
 BEGIN{
   if(match(orientation, "x"))      
@@ -26,18 +26,17 @@ BEGIN{
     {p1=37; pbox1=21; pbox2=1; pbox3=11;}
   len=substr(boxlengthline, pbox1, 10); len=len+0;
   araalen1=substr(boxlengthline, pbox2, 10); araalen1=araalen1+0;
-  # araalen2=
-  z1=4; 
-  z2=8;   # z2=substr(boxlengthline, pbox3, 10); araalen2=araalen2+0;
+  z1=3; 
+  z2=8;   # z2=substr(boxlengthline, pbox3, 10); z2=z2+0;
   araalen2=z2-z1;
   area=araalen1*araalen2;
   count=0; acceleration=0;
   thick=2*len;
 }
 /OW/{
-  coord=substr($0, p1, 8);  coord=coord+0;     # 运动方向（y）限制
-  coord2=substr($0, 37, 8); coord2=coord2+0;   # z方向限制
-  if( coord>(len-thick) || coord<thick )
+  coord=substr($0, p1, 8);   coord=coord+0;     # 运动方向（y）限制
+  coord2=substr($0, 37, 8);  coord2=coord2+0;   # z方向限制
+  if( coord>(len-thick) && coord<thick+5 )
   { 
     if( coord2>z1 && coord2<z2 )
     {

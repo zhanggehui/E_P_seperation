d15a39194f451b84ed9848eeece2fbe56429f21e
zhanggehui <395510548@qq.com>
2021-08-27 12:44:43 +0800

    update

--- gmx/pressure_on_water.sh
+++ gmx/pressure_on_water.sh
@@ -26,8 +26,8 @@ BEGIN{
     {p1=37; pbox1=21; pbox2=1; pbox3=11;}
   len=substr(boxlengthline, pbox1, 10); len=len+0;
   araalen1=substr(boxlengthline, pbox2, 10); araalen1=araalen1+0;
-  z1=3; 
-  z2=8;   # z2=substr(boxlengthline, pbox3, 10); z2=z2+0;
+  z1=0; 
+  z2=substr(boxlengthline, pbox3, 10); z2=z2+0;
   araalen2=z2-z1;
   area=araalen1*araalen2;
   count=0; acceleration=0;
@@ -38,7 +38,7 @@ BEGIN{
   coord2=substr($0, 37, 8);  coord2=coord2+0;   # z方向限制
   if( coord>(len-thick) && coord<thick+5 )
   { 
-    if( coord2>z1 && coord2<z2 )
+    if( coord2>-1 && coord2<11 )
     {
       count++; 
       serial=substr($0,16,5);

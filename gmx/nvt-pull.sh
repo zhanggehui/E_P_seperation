9f2fa79af2af888ccb4cf4b385d624c7f04f97cb
zhanggehui <395510548@qq.com>
2021-08-28 18:30:29 +0800

    update

--- gmx/nvt-pull.sh
+++ gmx/nvt-pull.sh
@@ -1,2 +1,2 @@
-gmx grompp -f $scriptsdir/nvt-pull.mdp -c ../nvtequ/nvt-equ.gro -p ../GO_ion_pp.top -o nvt-pull.tpr -n ../waterlayer.ndx #-maxwarn 1
+$gmx grompp -f $scriptsdir/nvt-pull.mdp -c ../nvtequ/nvt-equ.gro -p ../GO_ion_pp.top -o nvt-pull.tpr -n ../waterlayer.ndx #-maxwarn 1
 $gmxrun -v -deffnm nvt-pull

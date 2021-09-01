238d64a291ba2d961d4e393616bdb65a3521f732
zhanggehui <395510548@qq.com>
2021-09-01 21:06:19 +0800

    update

--- gmx/nvt-cycle.sh
+++ gmx/nvt-cycle.sh
@@ -5,7 +5,7 @@ dt=0.001             # 单位ps
 nsteps=10000000
 pressure=0           # 单位Mpa
 acc_water=all        # 根据具体情况记得修改，all/notall
-nlays=4
+nlays=20
 
 # --------------------------------------------------------------------- #
 echo "pressure: $pressure" > cyclelog

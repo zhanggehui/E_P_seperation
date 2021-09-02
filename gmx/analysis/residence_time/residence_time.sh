5ad5be67f2e210362305b200fee61e2560e5c415
zhanggehui <395510548@qq.com>
2021-09-02 15:01:28 +0800

    update

--- gmx/analysis/residence_time/residence_time.sh
+++ gmx/analysis/residence_time/residence_time.sh
@@ -5,7 +5,7 @@ cd ./rest_select
 # echo -e "\"1st shell\" resname SOL and name OW and within ${first_shell[$ion]} of group $ion\n" | \
 # $gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 10000
 
-$gmx gmx trajectory -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx \
+$gmx trajectory -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx \
 -b 0 -e 10000 -select "\"in_shell\" resname SOL and name OW and within 1.0 of atomnr 4553" 
 
 

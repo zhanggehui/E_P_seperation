b5f8795f11345e79c78134b80c9e642e40613bfa
zhanggehui <395510548@qq.com>
2021-09-02 15:00:22 +0800

    update

--- gmx/analysis/residence_time/residence_time.sh
+++ gmx/analysis/residence_time/residence_time.sh
@@ -1,11 +1,13 @@
 mkdir ./rest_select
 cd ./rest_select
 
-declare -A first_shell=(["LI"]="0.278" ["NA"]="0.32" ["K"]="0.354" ["CS"]="0.396")
+# declare -A first_shell=(["LI"]="0.278" ["NA"]="0.32" ["K"]="0.354" ["CS"]="0.396")
 # echo -e "\"1st shell\" resname SOL and name OW and within ${first_shell[$ion]} of group $ion\n" | \
+# $gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 10000
 
-echo -e "\"1st shell\" resname SOL and name OW and within 1.0 of atomnr 4553\n" | \
-$gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 10000
+$gmx gmx trajectory -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx \
+-b 0 -e 10000 -select "\"in_shell\" resname SOL and name OW and within 1.0 of atomnr 4553" 
+
 
 cd ../
 mv ./rest_select ../$rundir/${pressure}Mpa-${voltage}V

c6323292b86591c5ca0ad3001f4bb2538a668800
zhanggehui <395510548@qq.com>
2021-09-02 14:53:15 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -4,15 +4,15 @@ source $scriptsdir/rdf/rdf_func.sh
 
 cd ../
 
-# 正确启动python环境，由于使用了intel编译器的环境，破坏了原有的python环境
+# 正确启动python环境，如果使用了intel编译器的环境会破坏了原有的python环境，使用source deactivate
 # source deactivate
 source /home/liufeng_pkuhpc/anaconda3/bin/activate base
 python --version
 
 ion=${rundir%%_*}
-for ((i=0; i<=15; i=i+1)); do
+for ((i=15; i<=15; i=i+1)); do
     pressure=`awk -v i=$i 'BEGIN{printf("%s", 100*i);}'`
-    for ((j=15; j<=15; j=j+1)); do 
+    for ((j=0; j<=15; j=j+1)); do 
         voltage=`awk -v j=$j 'BEGIN{printf("%s", 0.1*j);}'`
         if [ $i -eq 0 ] || [ $j -eq 0 ]; then
         # if [ $i -eq 15 ]; then
@@ -22,13 +22,13 @@ for ((i=0; i<=15; i=i+1)); do
                 cd ./$dir
                     # 统计离子位移
                     # source $scriptsdir/trajectory/traj_first_last.sh
-                    source $scriptsdir/trajectory/traj_continuous.sh
+                    # source $scriptsdir/trajectory/traj_continuous.sh
 
                     # 统计径向分布函数
                     # source $scriptsdir/rdf/rdf.sh
                     
                     # 水合层滞留时间
-                    # source $scriptsdir/residence_time/residence_time.sh
+                    source $scriptsdir/residence_time/residence_time.sh
                     
                     # 速度分布
                     # source $scriptsdir/velocity/velocity_profile.sh
--- gmx/analysis/residence_time/residence_time.sh
+++ gmx/analysis/residence_time/residence_time.sh
@@ -2,8 +2,10 @@ mkdir ./rest_select
 cd ./rest_select
 
 declare -A first_shell=(["LI"]="0.278" ["NA"]="0.32" ["K"]="0.354" ["CS"]="0.396")
-echo -e "\"1st shell\" resname SOL and name OW and within ${first_shell[$ion]} of group $ion\n" | \
-gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 10000
+# echo -e "\"1st shell\" resname SOL and name OW and within ${first_shell[$ion]} of group $ion\n" | \
+
+echo -e "\"1st shell\" resname SOL and name OW and within 1.0 of atomnr 4553\n" | \
+$gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 0 -e 10000
 
 cd ../
 mv ./rest_select ../$rundir/${pressure}Mpa-${voltage}V

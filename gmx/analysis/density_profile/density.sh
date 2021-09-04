e30598d69675938b6bd1dff1bd8f637f229491b8
zhanggehui <395510548@qq.com>
2021-09-04 21:21:55 +0800

    update

--- gmx/analysis/density_profile/density.sh
+++ gmx/analysis/density_profile/density.sh
@@ -1,7 +1,7 @@
 dir=${pressure}Mpa-${voltage}V
 mkdir ./$dir
 
-echo -e "a OW\nr SOL & ! a OW\nname 12 HW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
+echo -e "a OW\nr SOL & ! a OW\nname 12 HW\ndel 8\ndel 8\ndel 8\nq" | $gmx make_ndx -f nvt-production.gro
 indexfile=index.ndx
 get_density_along_z OW ./$dir/OW.xvg
 get_density_along_z HW ./$dir/HW.xvg

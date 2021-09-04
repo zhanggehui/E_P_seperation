8a347166536a62a95519541f2df2281b90d74106
zhanggehui <395510548@qq.com>
2021-09-04 21:06:22 +0800

    update

--- gmx/analysis/density_profile/density.sh
+++ gmx/analysis/density_profile/density.sh
@@ -1,9 +1,14 @@
 dir=${pressure}Mpa-${voltage}V
 mkdir ./$dir
+
 echo -e "a OW\nr SOL & ! a OW\nname 12 HW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
-n_bins=600
-get_density_along_z $ion ./$dir/ion.xvg ${n_bins}
-get_density_along_z OW ./$dir/OW.xvg ${n_bins}
-get_density_along_z HW ./$dir/HW.xvg ${n_bins}
+indexfile=index.ndx
+get_density_along_z OW ./$dir/OW.xvg
+get_density_along_z HW ./$dir/HW.xvg
+
+indexfile=waterlayer.ndx
+get_density_along_z $ion ./$dir/ion.xvg
+get_density_along_z funcgrp ./$dir/funcgrp.xvg
+
 rm -rf \#* index.ndx
 mv ./$dir ../$rundir/$dir
--- gmx/analysis/density_profile/density_func.sh
+++ gmx/analysis/density_profile/density_func.sh
@@ -1,3 +1,3 @@
 function get_density_along_z() {
-    echo "$1" | gmx density -f nvt-production.trr -n index.ndx -s nvt-production.tpr -dens number -d Z -o $2 -b 1000 -e 10000 -sl $3
+    echo "$1" | gmx density -f nvt-production.trr -n $indexfile -s nvt-production.tpr -dens number -d Z -o $2 -b 1000 -e 10000 -sl 400
 }

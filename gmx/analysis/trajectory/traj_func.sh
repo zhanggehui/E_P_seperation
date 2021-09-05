723ce8e0140278348fdafc270419e8e492c5ad5a
zhanggehui <395510548@qq.com>
2021-09-05 20:37:25 +0800

    update

--- gmx/analysis/trajectory/traj_func.sh
+++ gmx/analysis/trajectory/traj_func.sh
@@ -13,13 +13,18 @@ function get_first_and_last_frame() {
 }
 
 function get_continuous_frame() {
+    
+    # 1.
     # 用于gmx select的例子
     # echo 'name OW and z { 3 to 7 }' z在3-7nm之间的OW分子
     # echo 'name OW NA' 
     # echo 'group Water'
     
-    echo "name $ion" | $gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
-    $gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $1 -b $2 -e $3 -n index.ndx -dt $4 -pbc nojump
+    # echo "name $ion" | $gmx select -f nvt-production.trr -s nvt-production.tpr -b 0 -e 0 -on
+    # $gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $1 -b $2 -e $3 -n index.ndx -dt $4 -pbc nojump
+
+    # 2.
+    echo "$1" | $gmx trjconv -f nvt-production.trr -s nvt-production.tpr -o $2 -pbc nojump -b $3 -e $4 -n waterlayer.ndx
 
     rm -rf \#* 
     if [ -e index.ndx ]; then

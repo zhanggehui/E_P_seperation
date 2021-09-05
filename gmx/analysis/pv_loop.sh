eb628e7a74d1262a9e5e63a0960f2ae1a8c05c29
zhanggehui <395510548@qq.com>
2021-09-05 19:18:51 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -21,7 +21,7 @@ for ((i=0; i<=15; i=i+1)); do
                 echo "-------------------------- ${pressure}Mpa-${voltage}V --------------------------"
                 cd ./$dir
                     # 统计离子位移
-                    # source $scriptsdir/trajectory/traj_first_last.sh
+                    source $scriptsdir/trajectory/traj_first_last.sh
                     # source $scriptsdir/trajectory/traj_continuous.sh
 
                     # 统计径向分布函数
@@ -34,7 +34,7 @@ for ((i=0; i<=15; i=i+1)); do
                     # source $scriptsdir/velocity/velocity_profile.sh
 
                     # 密度分布
-                    source $scriptsdir/density_profile/density.sh
+                    # source $scriptsdir/density_profile/density.sh
                     
                     # 水合层角度分布
                     # source $scriptsdir/angle_distribution/theta.sh

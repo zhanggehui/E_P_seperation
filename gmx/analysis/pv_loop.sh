e49e113e16403d7e2d2138723718cfd040ba8709
zhanggehui <395510548@qq.com>
2021-09-16 09:06:08 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -5,8 +5,9 @@ source $scriptsdir/rdf/rdf_func.sh
 cd ../
 
 # 正确启动python环境，如果使用了intel编译器的环境会破坏了原有的python环境，使用source deactivate
-source deactivate
-source /home/liufeng_pkuhpc/anaconda3/bin/activate base
+conda activate base
+# source deactivate
+# source /home/liufeng_pkuhpc/anaconda3/bin/activate base
 python --version
 
 ion=${rundir%%_*}
@@ -15,7 +16,7 @@ for ((i=0; i<=15; i=i+1)); do
     for ((j=0; j<=15; j=j+1)); do 
         voltage=`awk -v j=$j 'BEGIN{printf("%s", 0.1*j);}'`
         if [ $i -eq 0 ] || [ $j -eq 0 ]; then
-        # if [ $i -eq 1 ] || [ $j -eq 15 ]; then
+        # if [ $i -eq 15 ] || [ $j -eq 15 ]; then
             dir=${pressure}Mpa-${voltage}V
             if [ -d $dir ]; then
                 echo "-------------------------- ${pressure}Mpa-${voltage}V --------------------------"
@@ -31,10 +32,10 @@ for ((i=0; i<=15; i=i+1)); do
                     # source $scriptsdir/residence_time/residence_time.sh
                     
                     # 速度分布
-                    # source $scriptsdir/velocity/velocity_profile.sh
+                    source $scriptsdir/velocity/velocity_profile.sh
 
                     # 密度分布
-                    source $scriptsdir/density_profile/density.sh
+                    # source $scriptsdir/density_profile/density.sh
                     
                     # 水合层角度分布
                     # source $scriptsdir/angle_distribution/theta.sh

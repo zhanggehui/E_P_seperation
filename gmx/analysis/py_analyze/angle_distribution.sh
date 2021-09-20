3c4f43d7df441502aeb55803115f3a04957e4a9c
zhanggehui <395510548@qq.com>
2021-09-20 16:49:09 +0800

    update

--- gmx/analysis/py_analyze/angle_distribution.sh
+++ gmx/analysis/py_analyze/angle_distribution.sh
@@ -2,8 +2,8 @@ mkdir ./$rundir
 cd ./$rundir
 dir=/home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_GO/md_scripts/gmx/analysis/py_analyze
 
-echo -e "a OW or a ${ion}\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
-echo "OW_OR_A_${ion}" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
+echo -e "a OW or a ${ion}\nq" | $gmx make_ndx -f ../nvt-production.gro -quiet
+echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
 
 # echo -e "a OW | a 3948\nq" | gmx make_ndx -f ../nvt-production.gro -quiet
 # echo "OW_a_3948" | gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b ${tb} -e ${te} -n index.ndx -quiet

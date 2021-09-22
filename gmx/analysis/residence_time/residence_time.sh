902e333f9095ea44cb5a7bef4eddcd4efc4a5a0b
zhanggehui <395510548@qq.com>
2021-09-22 22:20:42 +0800

    update

--- gmx/analysis/residence_time/residence_time.sh
+++ gmx/analysis/residence_time/residence_time.sh
@@ -3,7 +3,7 @@ cd ./rest_select
 
 declare -A first_shell=(["LI"]="0.278" ["NA"]="0.32" ["K"]="0.354" ["CS"]="0.396")
 echo -e "\"1st shell\" resname SOL and name OW and within ${first_shell[$ion]} of group $ion\n" | \
-$gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 1000 -e 10000 # -nocumlt
+$gmx select -s ../nvt-production.tpr -f ../nvt-production.trr -n ../waterlayer.ndx -os -oc -oi -on -om -of -olt -b 1000 -e 10000 -norm # -nocumlt
 
 
 # echo -e "atomnr 4553 or resname SOL and within 1.5 of atomnr 4553\n" | \

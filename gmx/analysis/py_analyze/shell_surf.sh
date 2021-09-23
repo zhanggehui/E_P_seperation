29ef3822a314e20c0daca922923cab202aa2ef79
zhanggehui <395510548@qq.com>
2021-09-23 11:16:38 +0800

    update

--- gmx/analysis/py_analyze/shell_surf.sh
+++ gmx/analysis/py_analyze/shell_surf.sh
@@ -2,11 +2,12 @@ mkdir ./$rundir
 cd ./$rundir
 dir=/home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_GO/md_scripts/gmx/analysis/py_analyze
 
+out=/home/liufeng_pkuhpc/lustre3/ion_OW.gro
 echo -e "a OW or a ${ion}\nq" | $gmx make_ndx -f ../nvt-production.gro -quiet
-echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
+echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o $out -pbc nojump -b 1000 -e 10000 -n index.ndx -quiet
 
-python $dir/shell_surf.py -ingro ion_OW.gro -seltype OW -reftype ${ion}
+python $dir/shell_surf.py -ingro $out -seltype OW -reftype ${ion}
 
-rm -rf \#* index.ndx # ion_OW.gro
+rm -rf \#* index.ndx $out
 cd ../
 mv ./$rundir ../$rundir/${pressure}Mpa-${voltage}V

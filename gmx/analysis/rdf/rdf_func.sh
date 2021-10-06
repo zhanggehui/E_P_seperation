d86f43a78a836415432a26c6c918ed0bfa95f07f
zhanggehui <395510548@qq.com>
2021-10-06 23:03:25 +0800

    update

--- gmx/analysis/rdf/rdf_func.sh
+++ gmx/analysis/rdf/rdf_func.sh
@@ -1,5 +1,5 @@
 function get_rdf_and_coordnum() {
-    echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | gmx make_ndx -f nvt-production.gro
-    gmx rdf -f nvt-production.trr -n index.ndx -norm rdf -ref $1 -sel OW -selrpos atom -seltype atom -o $2 -cn $3 -b 1000 -e $4 -rmax 1
+    echo -e "a OW\ndel 8\ndel 8\ndel 8\nq" | $gmx make_ndx -f nvt-production.gro
+    $gmx rdf -f nvt-production.trr -n index.ndx -norm rdf -ref $1 -sel OW -selrpos atom -seltype atom -o $2 -cn $3 -b 1000 -e $4 -rmax 1
     rm -rf \#* index.ndx
 }

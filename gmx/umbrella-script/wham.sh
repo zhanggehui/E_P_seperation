87098c31fb0506b489316c9da973a09cb5121ef9
zhanggehui <395510548@qq.com>
2021-10-14 13:41:18 +0800

    update

--- gmx/umbrella-script/wham.sh
+++ gmx/umbrella-script/wham.sh
@@ -5,4 +5,4 @@ else
 fi
 
 $gmx wham -it ../tpr-files.dat -if ../pullf-files.dat -o -hist -unit kCal  # -is ../coordsel.dat
-# 该文件（coordsel.dat），行数对应分析的位置数，列数与反应坐标数目对应，1代表参与分析
+# coordsel.dat中，行数对应分析的位置数，列数与反应坐标数目对应，1代表参与分析

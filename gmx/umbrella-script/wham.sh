01bf406f714587fc48130918d7080bbca783fa36
zhanggehui <395510548@qq.com>
2021-11-09 20:45:22 +0800

    update

--- gmx/umbrella-script/wham.sh
+++ gmx/umbrella-script/wham.sh
@@ -4,5 +4,5 @@ else
     OMP_NUM_THREADS=20
 fi
 
-$gmx wham -it ../tpr-files.dat -if ../pullf-files.dat -o -hist -unit kCal  # -is ../coordsel.dat
+$gmx wham -it ../tpr-files.dat -if ../pullf-files.dat -o -hist -temp 300 -unit kT  # -is ../coordsel.dat
 # coordsel.dat中，行数对应分析的位置数，列数与反应坐标数目对应，1代表参与分析

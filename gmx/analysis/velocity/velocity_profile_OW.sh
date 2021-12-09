e2d4ae55136311293e78fc1cdea0621299cdaabf
zhanggehui <395510548@qq.com>
2021-12-09 11:53:40 +0800

    update

--- gmx/analysis/velocity/velocity_profile_OW.sh
+++ gmx/analysis/velocity/velocity_profile_OW.sh
@@ -5,6 +5,6 @@ echo -e "a OW\nq" | $gmx make_ndx -f nvt-production.gro
 get_continuous_frame OW $trajgro 1000 10000
 source $scriptsdir/velocity/get_velocity_profile_OW.sh z vy $trajgro 4
 
-rm -rf $trajgro index.gro
+rm -rf index.gro # $trajgro
 
 mv z-vy.xvg ../$rundir/${pressure}Mpa-${voltage}V.xvg

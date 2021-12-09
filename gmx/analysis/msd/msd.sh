97ba6539924e3f1e2e8fdc6e0a7ba44886a36534
zhanggehui <395510548@qq.com>
2021-12-09 19:28:19 +0800

    update

--- gmx/analysis/msd/msd.sh
+++ gmx/analysis/msd/msd.sh
@@ -1,1 +1,3 @@
-echo "3" | $gmx msd -f nvt-production.trr -s nvt-production.tpr -lateral z -mol
+echo -e "a 8980\nq" | $gmx make_ndx -f ../nvt-production.gro
+echo "a_8980" | $gmx msd -f nvt-production.trr -s nvt-production.tpr -lateral z -mol
+rm -rf index.ndx

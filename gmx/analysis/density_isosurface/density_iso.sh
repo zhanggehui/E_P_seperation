0a1bbcd4e7921a559697900577ce2f8d2a65cfc3
zhanggehui <395510548@qq.com>
2021-12-13 12:13:01 +0800

    update

--- gmx/analysis/density_isosurface/density_iso.sh
+++ gmx/analysis/density_isosurface/density_iso.sh
@@ -2,7 +2,7 @@ mkdir ./cube
 
 cd ./cube
 
-echo -e "a OW\na 8980\ndel 8\ndel 8\n\nq" | $gmx make_ndx -f ../nvt-production.gro
+echo -e "a OW\na 9115\ndel 8\ndel 8\n\nq" | $gmx make_ndx -f ../nvt-production.gro
 echo -e "10\n0\n" | $gmx trjconv -s ../nvt-production.tpr -f ../nvt-production.trr -n index.ndx -o md_cnt.xtc -center -pbc mol
 echo -e "9\n10\n" | $gmx spatial -s ../nvt-production.tpr -f md_cnt.xtc -n index.ndx -nab 10 -b 1000 -e 60000 -bin 0.02 -nodiv
 
--- gmx/analysis/msd/msd.sh
+++ gmx/analysis/msd/msd.sh
@@ -1,10 +1,10 @@
 mkdir ./msd
-
 cd ./msd
 
-echo -e "a 8980\nq" | $gmx make_ndx -f ../nvt-production.gro
-echo "a_8980" | $gmx msd -f ../nvt-production.trr -s ../nvt-production.tpr -lateral z -n index.ndx -b 10000 -e 20000 #-mol 
-rm -rf index.ndx
+# echo -e "a 9115\nq" | $gmx make_ndx -f ../nvt-production.gro
+# echo "a_9115" | $gmx msd -f ../nvt-production.trr -s ../nvt-production.tpr -lateral z -n index.ndx -b 10000 -e 20000
+echo "3" | $gmx msd -f ../nvt-production.trr -s ../nvt-production.tpr -lateral z -b 10000 -e 20000 -mol 
 
+rm -rf index.ndx
 cd ../
-mv ./msd ../$rundir/${pressure}Mpa-${voltage}V
+mv ./msd/msd.xvg ../$rundir/$ion.xvg

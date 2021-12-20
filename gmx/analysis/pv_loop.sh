f94d432f35271f2a873c1c4dcf50644c393a6869
zhanggehui <395510548@qq.com>
2021-12-20 15:16:01 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -15,21 +15,21 @@ for ((i=0; i<=15; i=i+1)); do
     pressure=`awk -v i=$i 'BEGIN{printf("%s", 100*i);}'`
     # pressure=`awk -v i=$i 'BEGIN{printf("%s", 10*i);}'`
     for ((j=8; j<=20; j=j+1)); do 
-        # voltage=`awk -v j=$j 'BEGIN{printf("%s", 0.1*j);}'`
-        voltage=`awk -v j=$j 'BEGIN{printf("%s", -0.05*j);}'`
+        voltage=`awk -v j=$j 'BEGIN{printf("%s", 0.1*j);}'`
+        # voltage=`awk -v j=$j 'BEGIN{printf("%s", -0.05*j);}'`
         
-        if [ $i -eq 15 ] || [ $j -eq 15 ]; then
-        # if [ $i -eq 0 ] || [ $j -eq 0 ]; then
+        # if [ $i -eq 15 ] || [ $j -eq 15 ]; then
+        if [ $i -eq 0 ] || [ $j -eq 0 ]; then
             dir=${pressure}Mpa-${voltage}V
             if [ -d $dir ]; then
                 echo "-------------------------- ${pressure}Mpa-${voltage}V --------------------------"
                 cd ./$dir
                     # 统计离子位移
-                    source $scriptsdir/trajectory/traj_first_last.sh
+                    # source $scriptsdir/trajectory/traj_first_last.sh
                     # source $scriptsdir/trajectory/traj_continuous.sh
 
                     # 统计径向分布函数
-                    # source $scriptsdir/rdf/rdf.sh
+                    source $scriptsdir/rdf/rdf.sh
                     
                     # 水合层滞留时间
                     # source $scriptsdir/residence_time/residence_time.sh

8bd983dec1f03a8143eeed05195bdbea199e6a96
zhanggehui <395510548@qq.com>
2021-12-21 09:25:44 +0800

    update

--- gmx/analysis/py_analyze/density_map.sh
+++ gmx/analysis/py_analyze/density_map.sh
@@ -2,11 +2,11 @@ mkdir ./$rundir
 cd ./$rundir
 dir=/home/liufeng_pkuhpc/lustre3/zgh/gmx/gmx_GO/md_scripts/gmx/analysis/py_analyze
 
-echo -e "a OW or a ${ion}\nq" | $gmx make_ndx -f ../nvt-production.gro -quiet
-echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 60000 -n index.ndx -quiet
-echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o last.gro -pbc nojump -b 60000 -e 60000 -n index.ndx -quiet
+# echo -e "a OW or a ${ion}\nq" | $gmx make_ndx -f ../nvt-production.gro -quiet
+# echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion_OW.gro -pbc nojump -b 1000 -e 60000 -n index.ndx -quiet
+echo "OW_OR_A_${ion}" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o last.gro -pbc nojump -b 0 -e 60000 -skip 60000 -n index.ndx -quiet
 
-python $dir/density_map.py -ingro ion_OW.gro -seltype OW -reftype ${ion}
+# python $dir/density_map.py -ingro ion_OW.gro -seltype OW -reftype ${ion}
 
 rm -rf \#* index.ndx ion_OW.gro
 cd ../

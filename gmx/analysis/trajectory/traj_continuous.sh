b4e8c617139e278ae3fe7040c2edaf182361fd29
zhanggehui <395510548@qq.com>
2021-12-24 19:24:36 +0800

    update

--- gmx/analysis/trajectory/traj_continuous.sh
+++ gmx/analysis/trajectory/traj_continuous.sh
@@ -1,3 +1,11 @@
-trajgro=${pressure}Mpa-${voltage}V.gro
-get_continuous_frame $ion $trajgro 0 60000
-mv $trajgro ../$rundir/
+# trajgro=${pressure}Mpa-${voltage}V.gro
+# get_continuous_frame $ion $trajgro 0 60000
+# mv $trajgro ../$rundir/
+
+mkdir con_trajs
+echo -e "a 4553\na 9115\na 13677\nq" | $gmx make_ndx -f ./nvt-production.gro
+get_continuous_frame a_4553 a_4553.gro 0 10000
+get_continuous_frame a_9115 a_9115.gro 0 10000
+get_continuous_frame a_13677 a_13677.gro 0 10000
+cd ../
+mv ./con_trajs ../$rundir/

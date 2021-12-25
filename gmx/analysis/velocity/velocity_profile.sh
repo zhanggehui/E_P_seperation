6c9b79af5ef45093f393a8396cd9c9c45e1b7ed0
zhanggehui <395510548@qq.com>
2021-12-25 17:07:57 +0800

    update

--- gmx/analysis/velocity/velocity_profile.sh
+++ gmx/analysis/velocity/velocity_profile.sh
@@ -1,15 +1,17 @@
-trajgro=${pressure}Mpa-${voltage}V.gro
+mkdir ${pressure}Mpa-${voltage}V
+cd ./${pressure}Mpa-${voltage}V
+
+echo -e "a OW\nq" | $gmx make_ndx -f ../nvt-production.gro
+echo "OW" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o OW.gro -b 1000 -e 60000 -n index.ndx
+source $scriptsdir/velocity/get_velocity_profile.sh z vy OW.gro 4 OW
+rm -rf index.gro OW.gro
+mv z-vy.xvg OW.xvg
 
 
-# echo -e "a OW\nq" | $gmx make_ndx -f nvt-production.gro
-# get_continuous_frame OW $trajgro 1000 10000
-# source $scriptsdir/velocity/get_velocity_profile.sh z vy $trajgro 4 OW
-# rm -rf index.gro $trajgro
+echo "$ion" | $gmx trjconv -f ../nvt-production.trr -s ../nvt-production.tpr -o ion.gro -b 1000 -e 60000 -n ../waterlayer.ndx
+source $scriptsdir/velocity/get_velocity_profile.sh z vy ion.gro 4 $ion 
+rm -rf Ion.gro
+mv z-vy.xvg ion.xvg
 
-
-get_continuous_frame $ion $trajgro 1000 60000
-source $scriptsdir/velocity/get_velocity_profile.sh z vy $trajgro 4 $ion 
-rm -rf $trajgro
-
-
-mv z-vy.xvg ../$rundir/${pressure}Mpa-${voltage}V.xvg
+cd ../
+mv ${pressure}Mpa-${voltage}V ../$rundir/

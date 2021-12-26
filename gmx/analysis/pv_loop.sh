f48a61a3b5b86b7a3e5f1b3df57cf034c9d56f2c
zhanggehui <395510548@qq.com>
2021-12-26 11:46:08 +0800

    update

--- gmx/analysis/pv_loop.sh
+++ gmx/analysis/pv_loop.sh
@@ -6,27 +6,27 @@ cd ../
 
 # 正确启动python环境，如果使用了intel编译器的环境会破坏了原有的python环境，使用source deactivate
 # conda activate base在计算节点不能使用
-source deactivate
-source /home/liufeng_pkuhpc/lustre2/anaconda3/bin/activate base
-python --version
+# source deactivate
+# source /home/liufeng_pkuhpc/lustre2/anaconda3/bin/activate base
+# python --version
 
 ion=${rundir%%_*}
-for ((i=0; i<=15; i=i+15)); do
+for ((i=0; i<=15; i=i+1)); do
     pressure=`awk -v i=$i 'BEGIN{printf("%s", 100*i);}'`
     # pressure=`awk -v i=$i 'BEGIN{printf("%s", 10*i);}'`
-    for ((j=0; j<=15; j=j+15)); do 
+    for ((j=0; j<=15; j=j+1)); do 
         voltage=`awk -v j=$j 'BEGIN{printf("%s", 0.1*j);}'`
         # voltage=`awk -v j=$j 'BEGIN{printf("%s", -0.05*j);}'`
         
-        if [ $i -eq 15 ] || [ $j -eq 15 ]; then
-        # if [ $i -eq 0 ] || [ $j -eq 0 ]; then
+        # if [ $i -eq 15 ] || [ $j -eq 15 ]; then
+        if [ $i -eq 0 ] || [ $j -eq 0 ]; then
         # if [ $i -eq 15 ]; then
             dir=${pressure}Mpa-${voltage}V_60ns
             if [ -d $dir ]; then
                 echo "-------------------------- ${pressure}Mpa-${voltage}V --------------------------"
                 cd ./$dir
                     # 统计离子位移
-                    # source $scriptsdir/trajectory/traj_first_last.sh
+                    source $scriptsdir/trajectory/traj_first_last.sh
                     # source $scriptsdir/trajectory/traj_continuous.sh
 
                     # 统计径向分布函数
@@ -36,7 +36,7 @@ for ((i=0; i<=15; i=i+15)); do
                     # source $scriptsdir/residence_time/residence_time.sh
                     
                     # 速度分布
-                    source $scriptsdir/velocity/velocity_profile.sh
+                    # source $scriptsdir/velocity/velocity_profile.sh
 
                     # 密度分布
                     # source $scriptsdir/density_profile/density.sh
